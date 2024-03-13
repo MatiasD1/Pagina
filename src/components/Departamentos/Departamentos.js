1e3a83e84431e2a0bcbc382a9269cb24215bd181
matias durand <mati.durand98@gmail.com>
2024-03-13 10:42:00 -0300

    Estructura básica de Departamentos

--- src/components/Departamentos/Departamentos.js
+++ src/components/Departamentos/Departamentos.js
@@ -4,6 +4,36 @@ const Departamentos = () => {
     return (
         <div className='departamentos'>
             <p>Departamentos</p>
+                <div class="containerDepartamentos">
+                    <div class="item">
+                        <img src="https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2022/02/DC-Comics-Cuanto-tiempo-lleva-activo-Bruce-Wayne-como-el-Caballero-de-la-Noche-en-The-Batman.png?fit=1280%2C720&quality=80&ssl=1" alt="Imagen 1"/>
+                        <div class="textoDepartamentos">
+                                <h2>Titulo</h2>
+                                <p>Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 </p>
+                            </div>
+                    </div>
+                    <div class="item">
+                        <div class="textoDepartamentos">
+                            <h2>Titulo</h2>
+                            <p>Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 </p>
+                        </div>      
+                        <img src="https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2022/02/DC-Comics-Cuanto-tiempo-lleva-activo-Bruce-Wayne-como-el-Caballero-de-la-Noche-en-The-Batman.png?fit=1280%2C720&quality=80&ssl=1" alt="Imagen 2"/>
+                    </div>
+                    <div class="item">
+                        <img src="https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2022/02/DC-Comics-Cuanto-tiempo-lleva-activo-Bruce-Wayne-como-el-Caballero-de-la-Noche-en-The-Batman.png?fit=1280%2C720&quality=80&ssl=1" alt="Imagen 3"/>
+                        <div class="textoDepartamentos">
+                            <h2>Titulo</h2>
+                            <p>Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 </p>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="textoDepartamentos">
+                                <h2>Titulo</h2>
+                                <p>Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 Texto 3 </p>
+                        </div>
+                        <img src="https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2022/02/DC-Comics-Cuanto-tiempo-lleva-activo-Bruce-Wayne-como-el-Caballero-de-la-Noche-en-The-Batman.png?fit=1280%2C720&quality=80&ssl=1" alt="Imagen 4"/>
+                    </div>
+                </div>
         </div>
     
     );
